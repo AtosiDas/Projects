44c429af497e5bf88fcfa510d5afc9f8bdd39fa3
Atosi <atosidas44@gmail.com>
2023-07-18 20:05:45 +0530

    Add some state variables

--- Lottery.sol
+++ Lottery.sol
@@ -1,3 +1,13 @@
 //SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+contract Lottery {
+    address public Manager;
+    address payable[] public participants;
+    uint EndTime = block.timestamp + 2 minutes;
+    
+    constructor(){
+        Manager = msg.sender;
+    }
+
+}
