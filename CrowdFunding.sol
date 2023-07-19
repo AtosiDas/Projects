b557b6a4d46ea45473aa6a6a62cf8a98f649ae8c
Atosi <atosidas44@gmail.com>
2023-07-19 23:34:17 +0530

    Add some state variables

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -1,3 +1,23 @@
 //SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+contract Crowdfunding {
+    mapping(address => uint) public contributors;
+    address public manager;
+    uint public MinContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address => bool) voters;
+    }
+    mapping(uint => Request) public requests;
+    uint public numRequests;
+}
